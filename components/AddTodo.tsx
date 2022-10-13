beb4e728313051c80cdc87145dc612702d8ff8f3
mrevanzak <mrevanzak@gmail.com>
2022-10-13 20:33:32 +0700

    fix: styling on button for IOS

--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -1,38 +1,50 @@
 import React from "react";
-import { Button, TextInput, View } from "react-native";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useState } from "react";
 
 interface Props {
-  submitHandler(text: string): void;
+    submitHandler(text: string): void;
 }
 
 export default function AddTodo({ submitHandler }: Props) {
-  const [text, setText] = useState("");
+    const [text, setText] = useState("");
 
-  const changeHandler = (text: string) => {
-    setText(text);
-  };
+    const changeHandler = (text: string) => {
+        setText(text);
+    };
 
-  const pressHandler = () => {
-    submitHandler(text);
-    setText("");
-  };
+    const pressHandler = () => {
+        submitHandler(text);
+        setText("");
+    };
 
-  return (
-    <View
-      style={{
-        marginBottom: 5,
-        flexBasis: "auto",
-        flexShrink: 1,
-        flexGrow: 0,
-      }}
-    >
-      <TextInput
-        placeholder="What do you want to do today? ..."
-        onChangeText={changeHandler}
-        value={text}
-      />
-      <Button title="add todo" color="#bd93f9" onPress={pressHandler} />
-    </View>
-  );
+    return (
+        <View
+            style={{
+                marginBottom: 5,
+                flexBasis: "auto",
+                flexShrink: 1,
+                flexGrow: 0,
+            }}
+        >
+            <TextInput
+                placeholder="What do you want to do today? ..."
+                onChangeText={changeHandler}
+                value={text}
+            />
+            <TouchableOpacity
+                style={{
+                    borderRadius: 5,
+                    padding: 10,
+                    marginVertical: 5,
+                    backgroundColor: "#bd93f9",
+                }}
+                onPress={pressHandler}
+            >
+                <Text style={{ color: "white", textAlign: "center" }}>
+                    ADD TODO
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
